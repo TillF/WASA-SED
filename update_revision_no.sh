c51f6fff4eee8f3147a9c68425c82e25bcc14184
Tobias Pilz <tpilz@users.noreply.github.com>
2014-07-28 12:01:13 +0000

    relevant for receiving version information under Linux: corrected shell script update_revision_no.sh

--- update_revision_no.sh
+++ update_revision_no.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #update file General/svn_rev.var with current revision numbering to be included in the binary
+#execute within the svn directory where you downloaded WASA-SED files into, otherwise it won't work
 REV_NO=`svnversion`
 if [ -z `echo $REV_NO | grep M` ]
 then
@@ -12,4 +13,4 @@ REP_DATE=`svn info | grep --only-matching "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9]
 DATE=`date`
 echo "!this file is updated on calling update_revision_no.sh to create svn_rev.txt which is linked to the source code
 rev_string1='$REV_NO $MODS'
-rev_string2='repository date $REP_DATE, built 2011/03/18 10:28:53'" > General/svn_rev.var
+rev_string2='repository date $REP_DATE, built $DATE'" > General/svn_rev.var
