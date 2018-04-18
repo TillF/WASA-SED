b861217d34385e23bfaaaa17d7d822d23436202e
Till Francke <francke@uni-potsdam.de>
2018-04-18 15:07:56 +0200

    computationally irrelevant: fixed make process in Windows, which did not include the repositpry date information anymore

--- src/update_revision_no.bat
+++ src/update_revision_no.bat
@@ -18,12 +18,13 @@ rem set output of git to variable
 rem for /f %%i in ('git show -s --format^=%%ci HEAD') do set gitdate=%%i 
 rem for /f "delims=" %%a in ('git show -s --format^=%%ci %1^^') do set hours=!hours! "%%a"
 for /f "delims=" %%a in ('git show -s --format^=%%ci ^%1^') do set gitdate=%%a
+rem for /f "delims=" %%a in ('git show -s --format^=%%ci ^%1') do set gitdate=%%a
 
 rem for /f "delims=" %a in ('git show -s --format ^=%ci') do set hours=!hours! "%a"
 
 for /f %%i in ('date /T') do set sysdate=%%i 
 for /f %%i in ('time /T') do set systime=%%i 
 
-echo rev_string2='repository date %gitdate%, built %sysdate%%systime%' >> src\General\svn_rev.f90
+echo rev_string2='repository date %gitdate%, built %sysdate%%systime%' >> General\svn_rev.f90
 
 
