ffc46682cf8f56d2f0103107f68b6fe36927c9d2
Till Francke <francke@uni-potsdam.de>
2018-10-15 16:25:26 +0200

    ci

--- .copy_dlls.sh
+++ .copy_dlls.sh
@@ -1,8 +1,59 @@
 #collect Windows dependencies (DLLs) to include in the deployment
 #dest_dir="./build/" #destination directory where to collect binary and DLLs
 dest_dir=$1 #destination directory where to collect binary and DLLs
-dlls=`objdump -x $dest_dir/wasa.exe |grep "DLL Name" | sed -e 's/^.* //g'` #find names of required DLLs
-for dll in $dlls
+dlls_l=`objdump -x $dest_dir/wasa.exe |grep "DLL Name" | sed -e 's/^.* //g'` #find names of required DLLs
+dlls_a=($dlls_l) #convert list to array
+dlls_l=`echo "${dlls_a[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '` #remove duplicates
+dlls_a=($dlls_l) #convert list to array
+
+while :
+do
+    # echo start loop 
+	# echo DLL: "${dlls_a[@]}"
+	# echo length: "${#dlls_a[@]}"
+	# echo DLL_l "$dlls_l"
+
+	secondary_dependencies_l=$dlls_l #list of second-order dependencies, initialized with DLLs found so far
+	for dll in $dlls_l
+	do
+	   #echo ""
+	   #echo Dependencies of $dll
+	   full_path=`which $dll`
+	   further_dlls=`objdump -x $full_path |grep "DLL Name" | sed -e 's/^.* //g'` #find names of required DLLs
+	   #echo further: $further_dlls
+	   #further_dlls=$further_dlls 
+	   secondary_dependencies_l+=" $further_dlls" #collect further dependencies
+	   #echo ""
+	   #echo secondary_dependencies_l: $secondary_dependencies_l
+	done   
+	echo ""
+	secondary_dependencies_a=($secondary_dependencies_l) #convert list to array
+	secondary_dependencies_l=`echo "${secondary_dependencies_a[@]}" | xargs | tr ' ' '\n'  | sort | uniq | tr '\n' ' '` #remove duplicates
+	secondary_dependencies_a=($secondary_dependencies_l) #convert list to array
+		
+	# echo DLL: "${dlls_a[@]}"
+	# echo length: "${#dlls_a[@]}"
+	# echo DLL_l "$dlls_l"
+	# echo "$dlls_l" > dll.txt
+#	read -n 1 KEY
+	# echo secondary_dependencies_a: "${secondary_dependencies_a[@]}"
+	# echo length: "${#secondary_dependencies_a[@]}"
+	# echo secondary_dependencies_l: "$secondary_dependencies_l"
+	# echo "$secondary_dependencies_l" > sec.txt
+	# read -n 1 KEY
+
+	if [ "$dlls_l" == "$secondary_dependencies_l" ]
+	then
+		echo same
+		break
+	else
+		echo different, next loop
+	    dlls_l=$secondary_dependencies_l 
+		dlls_a=($dlls_l) #convert list to array
+	fi
+done
+
+for dll in $dlls_l
 do
    echo $dll
    full_path=`which $dll`
