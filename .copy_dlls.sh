191889d733afac4a579a91c0d888ca0abd080466
TillF <francke@uni-potsdam.de>
2018-10-17 09:07:59 +0200

    CIRDP4

--- .copy_dlls.sh
+++ .copy_dlls.sh
@@ -9,29 +9,26 @@ dlls_a=($dlls_l) #convert list to array
 dlls_l=`echo "${dlls_a[@]}" | xargs | tr ' ' '\n' | sort | uniq | tr -s '\n\r' ' '` #remove duplicates
 dlls_a=($dlls_l) #convert list to array
 
-echo DLL_l_prior2:
-echo "$dlls_l"x
-
 
 while :
 do
     echo start loop 
 	#echo DLL: "${dlls_a[@]}"
 	# echo length: "${#dlls_a[@]}"
-	echo DLL_l_prior3:
-    echo "$dlls_l"x
+	#echo DLL_l_prior3:
+    #echo "$dlls_l"x
 
 	secondary_dependencies_l=$dlls_l #list of second-order dependencies, initialized with DLLs found so far
 	for dll in $dlls_l
 	do
-	   echo ""
+	   #echo ""
 	   echo Dependencies of $dll
 	   full_path=`which $dll`
-	   echo ""
-	   echo full path:$full_path
+	   #echo ""
+	   #echo full path:$full_path
 	   further_dlls=`objdump -x $full_path |grep "DLL Name" | sed -e 's/^.* //g'` #find names of required DLLs
-	   echo ""
-	   echo further: $further_dlls
+	   #echo ""
+	   #echo further: $further_dlls
 	   #further_dlls=$further_dlls 
 	   secondary_dependencies_l+=" $further_dlls" #collect further dependencies
 	   #echo ""
@@ -42,17 +39,6 @@ do
 	secondary_dependencies_l=`echo "${secondary_dependencies_a[@]}" | xargs | tr ' ' '\n'  | sort | uniq | tr -s '\n\r' ' '` #remove duplicates
 	secondary_dependencies_a=($secondary_dependencies_l) #convert list to array
 		
-	# echo DLL: "${dlls_a[@]}"
-	# echo length: "${#dlls_a[@]}"
-	# echo DLL_l "$dlls_l"
-	# echo "$dlls_l" > dll.txt
-#	read -n 1 KEY
-	# echo secondary_dependencies_a: "${secondary_dependencies_a[@]}"
-	# echo length: "${#secondary_dependencies_a[@]}"
-	# echo secondary_dependencies_l: "$secondary_dependencies_l"
-	# echo "$secondary_dependencies_l" > sec.txt
-	# read -n 1 KEY
-
 	if [ "$dlls_l" == "$secondary_dependencies_l" ]
 	then
 		echo same
@@ -66,7 +52,6 @@ done
 
 echo DLLs to copy: "$dlls_l"
 
-exit
 for dll in $dlls_l
 do
    echo $dll
